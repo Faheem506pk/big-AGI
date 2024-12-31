003aa2221f6eaf8c81e2d2892a1d0868b6fe902e
faheem506pk <faheemiqbalm@gmail.com>
2024-12-31 16:00:12 +0500

    remove MenuButton from serach input

--- src/apps/chat/components/layout-drawer/ChatDrawer.tsx
+++ src/apps/chat/components/layout-drawer/ChatDrawer.tsx
@@ -156,13 +156,13 @@ function ChatDrawer(props: {
   const { isSearching } = isDrawerSearching(debouncedSearchQuery);
   const groupingComponent = React.useMemo(() => (
     <Dropdown>
-      <MenuButton
+      {/* <MenuButton
         aria-label='View options'
         slots={{ root: IconButton }}
         slotProps={{ root: { size: 'sm' } }}
       >
         <MoreVertIcon />
-      </MenuButton>
+      </MenuButton> */}
 
       {!isSearching ? (
         // Search/Filter default menu: Grouping, Filtering, ...

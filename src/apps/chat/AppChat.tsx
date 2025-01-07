04e8ebe4ede7b51153cdee58416d304477a8e8ff
faheem506pk <faheemiqbalm@gmail.com>
2025-01-07 19:04:17 +0500

    remove bordertop

--- src/apps/chat/AppChat.tsx
+++ src/apps/chat/AppChat.tsx
@@ -83,7 +83,7 @@ const composerOpenSx: SxProps = {
   zIndex: 21, // just to allocate a surface, and potentially have a shadow
   minWidth: { md: 480 }, // don't get compresses too much on desktop
   backgroundColor: themeBgAppChatComposer,
-  borderTop: `1px solid`,
+  // borderTop: `1px solid`,
   borderTopColor: 'rgba(var(--joy-palette-neutral-mainChannel, 99 107 116) / 0.4)',
   // hack: eats the bottom of the last message (as it has a 1px divider)
   mt: '-1px',
